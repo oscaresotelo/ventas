1339ca0b0dfeee069027639ba7a809c8fe585b33
oscarsotelo <oscaresotelo@gmail.com>
2024-05-15 10:44:28 -0300

    se

--- bakapp.py
+++ bakapp.py
@@ -19,7 +19,7 @@ whatsapp_icon = '''
 </a>
 '''
 st.markdown(whatsapp_icon, unsafe_allow_html=True)
-st.markdown("<p style='text-align: left; margin-top: -10px;'>Entrega Zona Centrica</p>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: left; margin-top: -10px;'>Entrega Zona Centrica sin Cargo</p>", unsafe_allow_html=True)
 
 image = Image.open("nino.jpg")
 nueva_imagen = image.resize((200, 200))
